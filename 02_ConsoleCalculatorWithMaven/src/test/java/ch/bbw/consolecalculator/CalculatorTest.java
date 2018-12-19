b57c7b31105353d6f6a126d53688ae222391e51a
Janick Niederberger <janick.niederberger@protonmail.com>
2018-12-19 20:56:30 +0100

    add test calculator class with unittest for summe

---
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
@@ -0,0 +1,22 @@
+package ch.bbw.consolecalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test Class for Class Calculator
+ * 
+ * @author Janick Niederberger
+ * @version 0.0.1
+ */
+public class CalculatorTest {
+	Calculator testee;
+	
+	@Test
+	public void testSummeZweiPositiveIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.summe(10, 25) == 35);
+	}
+
+}
